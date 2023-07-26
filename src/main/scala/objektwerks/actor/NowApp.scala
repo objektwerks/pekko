056ed2966275cf9f63274fc42dd8a648e8c18ba3
objektwerks <objektwerks@runbox.com>
2023-07-26 09:56:31 -0400

    added now app

---
+++ src/main/scala/objektwerks/actor/NowApp.scala
@@ -0,0 +1,20 @@
+package objektwerks.actor
+
+import java.time.Instant
+
+import org.apache.pekko.actor.typed.ActorSystem
+import org.apache.pekko.actor.typed.scaladsl.Behaviors
+
+object NowApp {
+  def main(args: Array[String]): Unit = {
+    final case class Now(now: String = Instant.now.toString) extends Product with Serializable
+    val nowActor = Behaviors.receiveMessage[Now] {
+      case Now(now) =>
+        println(s"*** Now: $now")
+        Behaviors.same
+    }
+    val system = ActorSystem[Now](nowActor, "now-app")
+    system ! Now()
+    system.terminate()
+  }
+}
