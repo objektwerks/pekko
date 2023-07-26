8189048481de2daf3095789cda151b5ef7bd2ae3
objektwerks <objektwerks@runbox.com>
2023-07-26 11:23:39 -0400

    edited now app

--- src/main/scala/objektwerks/typed/NowApp.scala
+++ src/main/scala/objektwerks/typed/NowApp.scala
@@ -5,16 +5,13 @@ import java.time.Instant
 import org.apache.pekko.actor.typed.ActorSystem
 import org.apache.pekko.actor.typed.scaladsl.Behaviors
 
-object NowApp {
-  def main(args: Array[String]): Unit = {
-    final case class Now(now: String = Instant.now.toString) extends Product with Serializable
-    val nowActor = Behaviors.receiveMessage[Now] {
-      case Now(now) =>
-        println(s"*** Now: $now")
-        Behaviors.same
-    }
-    val system = ActorSystem[Now](nowActor, "now-app")
-    system ! Now()
-    system.terminate()
+@main def runNowApp: Unit =
+  final case class Now(now: String = Instant.now.toString) extends Product with Serializable
+  val nowActor = Behaviors.receiveMessage[Now] {
+    case Now(now) =>
+      println(s"*** Now: $now")
+      Behaviors.same
   }
-}
+  val system = ActorSystem[Now](nowActor, "now-app")
+  system ! Now()
+  system.terminate()
