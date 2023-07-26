56e359f6def820edf432c9cc19091bc4e757403d
objektwerks <objektwerks@runbox.com>
2023-07-26 15:13:09 -0400

    adding actor tests

--- src/test/scala/objektwerks/actor/SelectionTest.scala
+++ src/test/scala/objektwerks/actor/SelectionTest.scala
@@ -1,65 +1,58 @@
-package akka
+package objektwerks.actor
 
-import akka.actor._
-import akka.pattern._
-import akka.util.Timeout
+import org.apache.pekko.actor.*
+import org.apache.pekko.pattern.*
+import org.apache.pekko.util.Timeout
 
 import org.scalatest.BeforeAndAfterAll
 import org.scalatest.funsuite.AnyFunSuite
 
 import scala.concurrent.Await
-import scala.concurrent.duration._
+import scala.concurrent.duration.*
 import scala.language.postfixOps
 
 case object ToGrandParents
 case object ToParents
 case object ToChildren
 
-class GrandParents extends Actor with ActorLogging {
+class GrandParents extends Actor with ActorLogging:
   import context.dispatcher
 
   log.info(s"*** GrandParents created: $self")
-  implicit val timeout = Timeout(1 second)
+  given timeout: Timeout = Timeout(1 second)
   val parent = context.actorOf(Props[Parents](), name = "parents")
 
-  def receive: Receive = {
+  def receive: Receive =
     case ToGrandParents => sender() ! "grandparents"
     case ToParents => parent ? ToParents pipeTo sender(); ()
     case ToChildren => parent ? ToChildren pipeTo sender(); ()
-  }
-}
 
-class Parents extends Actor with ActorLogging {
+class Parents extends Actor with ActorLogging:
   import context.dispatcher
 
   log.info(s"*** Parents created: $self")
-  implicit val timeout = Timeout(1 second)
+  given timeout: Timeout = Timeout(1 second)
   val child = context.actorOf(Props[Children](), name = "children")
 
-  def receive: Receive = {
+  def receive: Receive =
     case ToParents => sender() ! "parents"
     case ToChildren => child ? ToChildren pipeTo sender(); ()
-  }
-}
 
-class Children extends Actor with ActorLogging {
+class Children extends Actor with ActorLogging:
   log.info(s"*** Children created: $self")
-  implicit val timeout = Timeout(1 second)
+  given timeout: Timeout = Timeout(1 second)
 
-  def receive: Receive = {
+  def receive: Receive =
     case ToChildren => sender() ! "children"
-  }
-}
 
-class SelectionTest extends AnyFunSuite with BeforeAndAfterAll {
-  implicit val timeout = Timeout(1 second)
+class SelectionTest extends AnyFunSuite with BeforeAndAfterAll:
+  given timeout: Timeout = Timeout(1 second)
   val system = ActorSystem.create("selection", Conf.config)
   val grandparents = system.actorOf(Props[GrandParents](), name = "grandparents")
 
-  override protected def afterAll(): Unit = {
+  override protected def afterAll(): Unit =
     Await.result(system.terminate(), 1 second)
     ()
-  }
 
   test("grand parents") {
     assert("grandparents" == Await.result( (system.actorSelection("/user/grandparents") ? ToGrandParents).mapTo[String], 1 second))
@@ -75,4 +68,3 @@ class SelectionTest extends AnyFunSuite with BeforeAndAfterAll {
   test("children") {
     assert("children" == Await.result( (system.actorSelection("/user/grandparents/parents/*") ? ToChildren).mapTo[String], 1 second))
   }
-}
