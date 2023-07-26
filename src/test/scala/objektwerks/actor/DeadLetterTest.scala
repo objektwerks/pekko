f6e5b96315a2fb394c7bda44677611f5277045b5
objektwerks <objektwerks@runbox.com>
2023-07-26 15:02:56 -0400

    adding actor tests

--- src/test/scala/objektwerks/actor/DeadLetterTest.scala
+++ src/test/scala/objektwerks/actor/DeadLetterTest.scala
@@ -1,37 +1,32 @@
-package akka
+package objektwerks.actor
 
-import akka.actor._
-import akka.util.Timeout
+import org.apache.pekko.actor.*
+import org.apache.pekko.util.Timeout
 import org.scalatest.BeforeAndAfterAll
 import org.scalatest.funsuite.AnyFunSuite
 
 import scala.concurrent.Await
-import scala.concurrent.duration._
+import scala.concurrent.duration.*
 import scala.language.postfixOps
 
-class Service extends Actor with ActorLogging {
-  def receive: Receive = {
+class Service extends Actor with ActorLogging:
+  def receive: Receive =
     case message: String => log.info(s"*** Service received: $message")
-  }
-}
 
-class Listener extends Actor with ActorLogging {
-  def receive: Receive = {
+class Listener extends Actor with ActorLogging:
+  def receive: Receive =
     case _: DeadLetter => log.info(s"*** Listener received dead letter!")
-  }
-}
 
-class DeadLetterTest extends AnyFunSuite  with BeforeAndAfterAll {
-  implicit val timeout = Timeout(1 second)
+class DeadLetterTest extends AnyFunSuite  with BeforeAndAfterAll:
+  given timeout: Timeout = Timeout(1 second)
   val system = ActorSystem.create("deadletter", Conf.config)
   val service = system.actorOf(Props[Service](), name = "service")
   val listener = system.actorOf(Props[Listener](), name = "listener")
   system.eventStream.subscribe(listener, classOf[DeadLetter])
 
-  override protected def afterAll(): Unit = {
+  override protected def afterAll(): Unit =
     Await.result(system.terminate(), 1 second)
     ()
-  }
 
   test("dead letter") {
     service ! "First message!"
@@ -40,4 +35,3 @@ class DeadLetterTest extends AnyFunSuite  with BeforeAndAfterAll {
     Thread.sleep(1000)
     service ! "Second message!"
   }
-}
