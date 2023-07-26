3d00b390b6ce3ff8d3029937b038f198b0f2be4b
objektwerks <objektwerks@runbox.com>
2023-07-26 07:10:48 -0400

    refactored greeter app test

--- src/test/scala/objektwerks/GreeterAppTest.scala
+++ src/test/scala/objektwerks/GreeterAppTest.scala
@@ -1,16 +1,15 @@
 package objektwerks
 
 import org.apache.pekko.actor.testkit.typed.scaladsl.ScalaTestWithActorTestKit
+import org.scalatest.funsuite.AnyFunSuiteLike
+
 import objektwerks.Greeter.Greet
 import objektwerks.Greeter.Greeted
-import org.scalatest.wordspec.AnyWordSpecLike
 
-class GreeterAppTest extends ScalaTestWithActorTestKit with AnyWordSpecLike:
-  "A Greeter" must {
-    "reply to greeted" in {
-      val replyProbe = createTestProbe[Greeted]()
-      val underTest = spawn(Greeter())
-      underTest ! Greet("Santa", replyProbe.ref)
-      replyProbe.expectMessage(Greeted("Santa", underTest.ref))
-    }
+class GreeterAppTest extends ScalaTestWithActorTestKit with AnyFunSuiteLike:
+  test("greeting") {
+    val replyProbe = createTestProbe[Greeted]()
+    val underTest = spawn(Greeter())
+    underTest ! Greet("Santa", replyProbe.ref)
+    replyProbe.expectMessage(Greeted("Santa", underTest.ref))
   }
