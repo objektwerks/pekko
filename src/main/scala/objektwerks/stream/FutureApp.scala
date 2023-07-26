7870d615b330d595629bd7ca9a770e067a3714be
objektwerks <objektwerks@runbox.com>
2023-07-26 11:17:17 -0400

    edited future app

--- src/main/scala/objektwerks/stream/FutureApp.scala
+++ src/main/scala/objektwerks/stream/FutureApp.scala
@@ -5,35 +5,32 @@ import com.typesafe.config.ConfigFactory
 import org.apache.pekko.actor.ActorSystem
 import org.apache.pekko.stream.scaladsl.{Sink, Source}
 
-import scala.concurrent.{Await, ExecutionContext, Future}
-import scala.concurrent.duration._
+import scala.concurrent.{Await, ExecutionContextExecutor, Future}
+import scala.concurrent.duration.*
 import scala.io.StdIn
 import scala.language.postfixOps
 
-object FutureApp {
-  def main(args: Array[String]): Unit = {
-    val numbers = 10
-    val parallelism = Runtime.getRuntime.availableProcessors
+@main def runFutureApp: Unit =
+  val numbers = 10
+  val parallelism = Runtime.getRuntime.availableProcessors
 
-    implicit val system: ActorSystem = ActorSystem.create("future-app", ConfigFactory.load("app.conf"))
-    implicit val dispatcher: ExecutionContext = system.dispatcher
-    println("*** akka system started")
+  given system: ActorSystem = ActorSystem.create("future-app", ConfigFactory.load("app.conf"))
+  given dispatcher: ExecutionContextExecutor = system.dispatcher
+  println("*** akka system started")
 
-    println(s"*** squaring numbers with mapAsync parallelism set to: $parallelism ...")
-    Source(1 to numbers)
-      .mapAsync(parallelism) { number =>
-        Future { // simulate async io
-          println(s"*** $number squared = ${number * number}")
-        }
+  println(s"*** squaring numbers with mapAsync parallelism set to: $parallelism ...")
+  Source(1 to numbers)
+    .mapAsync(parallelism) { number =>
+      Future { // simulate async io
+        println(s"*** $number squared = ${number * number}")
       }
-      .runWith(Sink.ignore)
-    println(s"*** once all squared numbers have been printed, depress RETURN key to shutdown app")
+    }
+    .runWith(Sink.ignore)
+  println(s"*** once all squared numbers have been printed, depress RETURN key to shutdown app")
 
-    StdIn.readLine()
+  StdIn.readLine()
 
-    Await.result(system.terminate(), 10 seconds)
-    println("*** akka system terminated")
-    println("*** see log at /target/app.log")
-    println("*** app shutdown")
-  }
-}
+  Await.result(system.terminate(), 10 seconds)
+  println("*** akka system terminated")
+  println("*** see log at /target/app.log")
+  println("*** app shutdown")
