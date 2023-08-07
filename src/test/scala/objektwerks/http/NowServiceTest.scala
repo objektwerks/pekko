22003de8b419737d1cc3b9c2c6369a2ec8857968
objektwerks <objektwerks@runbox.com>
2023-08-07 11:15:12 -0400

    edited now service test

--- src/test/scala/objektwerks/http/NowServiceTest.scala
+++ src/test/scala/objektwerks/http/NowServiceTest.scala
@@ -5,15 +5,17 @@ import com.typesafe.config.ConfigFactory
 import org.apache.pekko.actor.ActorSystem
 import org.apache.pekko.http.scaladsl.Http
 import org.apache.pekko.http.scaladsl.model.StatusCodes
+import org.apache.pekko.http.scaladsl.testkit.ScalatestRouteTest
 
 import org.scalatest.BeforeAndAfterAll
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AnyWordSpec
 
-class NowServiceTest extends AnyWordSpec with Matchers with BeforeAndAfterAll with NowService:
+class NowServiceTest extends AnyWordSpec with Matchers with ScalatestRouteTest with BeforeAndAfterAll with NowService:
   import org.apache.pekko.http.scaladsl.marshallers.sprayjson.SprayJsonSupport.*
+  import NowJsonCodecs.given
 
-  given system: ActorSystem = ActorSystem.create("now", ConfigFactory.load("test.conf"))
+  given ActorSystem = ActorSystem.create("now", ConfigFactory.load("test.conf"))
   val server = Http()
     .newServerAt("localhost", 0)
     .bindFlow(routes)
