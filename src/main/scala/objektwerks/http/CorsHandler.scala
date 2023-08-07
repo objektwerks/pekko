e73ac70e704390cd5aa43fd7382e2a6a7f8c895e
objektwerks <objektwerks@runbox.com>
2023-08-07 11:00:14 -0400

    added cors handler

---
+++ src/main/scala/objektwerks/http/CorsHandler.scala
@@ -0,0 +1,29 @@
+package objektwerks.http
+
+import org.apache.pekko.http.scaladsl.model.HttpMethods._
+import org.apache.pekko.http.scaladsl.model.headers._
+import org.apache.pekko.http.scaladsl.model.HttpResponse
+import org.apache.pekko.http.scaladsl.server.Directives._
+import org.apache.pekko.http.scaladsl.server.{Directive0, Route}
+
+/**
+ * See: https://dzone.com/articles/handling-cors-in-akka-http
+ */
+trait CorsHandler {
+  val corsResponseHeaders = List(
+    `Access-Control-Allow-Origin`.*,
+    `Access-Control-Allow-Credentials`(true),
+    `Access-Control-Allow-Headers`("Authorization", "Content-Type", "X-Requested-With")
+  )
+
+  def corsHandler(route: Route): Route = addAccessControlHeaders { preflightRequestHandler ~ route }
+
+  def addAccessControlHeaders: Directive0 = respondWithHeaders( corsResponseHeaders )
+
+  def preflightRequestHandler: Route = options {
+    complete( HttpResponse()
+      .withHeaders( `Access-Control-Allow-Methods`(GET, POST, PUT, PATCH, DELETE, OPTIONS) ) )
+  }
+
+  def addCORSHeaders(response: HttpResponse): HttpResponse = response.withHeaders( corsResponseHeaders )
+}
