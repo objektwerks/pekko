9dda52f5b64c1cd1a4664169d05d8a17b61c5f3a
Objektwerks <objektwerks@runbox.com>
2023-09-25 11:21:29 -0400

    Update SupervisorStrategyTest.scala

--- src/test/scala/objektwerks/actor/SupervisorStrategyTest.scala
+++ src/test/scala/objektwerks/actor/SupervisorStrategyTest.scala
@@ -15,26 +15,26 @@ sealed trait Task
 case object Play extends Task
 case object CleanRoom extends Task
 
-class CleanRoomException(cause: String) extends Exception(cause)
+final class CleanRoomException(cause: String) extends Exception(cause)
 
-class Nanny extends Actor with ActorLogging:
+final class Nanny extends Actor with ActorLogging:
   given timeout: Timeout = Timeout(3 seconds)
   val child = context.actorOf(Props[Child](), name = "child")
 
   override def supervisorStrategy: SupervisorStrategy =
     OneForOneStrategy(maxNrOfRetries = 1, withinTimeRange = 1 second) {
       case _: CleanRoomException => Restart
-  }
+    }
 
   def receive: Receive =
     case task: Task => child ! task
 
-class Child extends Actor with ActorLogging:
+final class Child extends Actor with ActorLogging:
   def receive: Receive =
     case Play => log.info("*** Child happily playing!")
     case CleanRoom => throw new CleanRoomException("Child refuses to clean room!")
 
-class SupervisorStrategyTest extends AnyFunSuite with BeforeAndAfterAll:
+final class SupervisorStrategyTest extends AnyFunSuite with BeforeAndAfterAll:
   given timeout: Timeout = Timeout(1 second)
   val system = ActorSystem.create("supervisor", Conf.config)
   val nanny = system.actorOf(Props[Nanny](), name = "nanny")
@@ -43,9 +43,8 @@ class SupervisorStrategyTest extends AnyFunSuite with BeforeAndAfterAll:
     Await.result(system.terminate(), 1 second)
     ()
 
-  test("nanny ! child") {
+  test("nanny ! child"):
     nanny ! Play
     nanny ! CleanRoom
     Thread.sleep(3000)
     nanny ! Play
-  }
