f975031ddb5c7286231e93cc7edac694b348ce53
Objektwerks <objektwerks@runbox.com>
2023-09-25 11:15:56 -0400

    Update DeadLetterTest.scala

--- src/test/scala/objektwerks/actor/DeadLetterTest.scala
+++ src/test/scala/objektwerks/actor/DeadLetterTest.scala
@@ -9,15 +9,15 @@ import scala.concurrent.Await
 import scala.concurrent.duration.*
 import scala.language.postfixOps
 
-class Service extends Actor with ActorLogging:
+final class Service extends Actor with ActorLogging:
   def receive: Receive =
     case message: String => log.info(s"*** Service received: $message")
 
-class Listener extends Actor with ActorLogging:
+final class Listener extends Actor with ActorLogging:
   def receive: Receive =
     case _: DeadLetter => log.info(s"*** Listener received dead letter!")
 
-class DeadLetterTest extends AnyFunSuite  with BeforeAndAfterAll:
+final class DeadLetterTest extends AnyFunSuite with BeforeAndAfterAll:
   given timeout: Timeout = Timeout(1 second)
   val system = ActorSystem.create("deadletter", Conf.config)
   val service = system.actorOf(Props[Service](), name = "service")
@@ -28,10 +28,9 @@ class DeadLetterTest extends AnyFunSuite  with BeforeAndAfterAll:
     Await.result(system.terminate(), 1 second)
     ()
 
-  test("dead letter") {
+  test("dead letter"):
     service ! "First message!"
     Thread.sleep(1000)
     service ! PoisonPill
     Thread.sleep(1000)
     service ! "Second message!"
-  }
