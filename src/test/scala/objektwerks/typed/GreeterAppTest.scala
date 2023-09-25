cb106e8e1732d95f16bda7459dc2e1fc6e9eb61c
Objektwerks <objektwerks@runbox.com>
2023-09-25 11:26:25 -0400

    Update GreeterAppTest.scala

--- src/test/scala/objektwerks/typed/GreeterAppTest.scala
+++ src/test/scala/objektwerks/typed/GreeterAppTest.scala
@@ -6,10 +6,9 @@ import org.scalatest.funsuite.AnyFunSuiteLike
 
 import objektwerks.typed.Greeter.{Greet, Greeted}
 
-class GreeterAppTest extends ScalaTestWithActorTestKit with AnyFunSuiteLike:
-  test("greeter") {
+final class GreeterAppTest extends ScalaTestWithActorTestKit with AnyFunSuiteLike:
+  test("greeter"):
     val replyProbe = createTestProbe[Greeted]()
     val underTest = spawn(Greeter())
     underTest ! Greet("Santa", replyProbe.ref)
     replyProbe.expectMessage(Greeted("Santa", underTest.ref))
-  }
